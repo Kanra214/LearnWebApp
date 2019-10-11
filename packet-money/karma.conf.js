f23453a402ca2e308e6198868dca2a74485c0a8c
Xiyun Zhang <zhxyun201709@163.com>
2019-10-11 03:58:34 -0400

    ehhhhhhhhh

--- packet-money/karma.conf.js
+++ packet-money/karma.conf.js
@@ -1,6 +1,7 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
-// process.env.CHROME_BIN = require('puppeteer').executablePath();
+process.env.PHANTOMJS_BIN='node_modules/phantomjs/lib/phantom/bin/phantomjs';
+;
 
 module.exports = function (config) {
   config.set({
