fe3441d2a4467cfd40e812e824d6bde200f4977a
Xiyun Zhang <zhxyun201709@163.com>
2020-05-24 09:52:09 -0400

    deleted old frontend

--- packet-money/src/app/valid-end-time.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ValidEndTimeDirective } from './valid-end-time.directive';
-
-describe('ValidEndTimeDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ValidEndTimeDirective();
-    expect(directive).toBeTruthy();
-  });
-});
