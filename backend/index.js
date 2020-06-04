fad058bfb6460d3894684c1a4ad4e9da6c8e4b3b
Xiyun Zhang <zhxyun201709@163.com>
2020-06-03 23:37:56 -0400

    add host in index.js

--- backend/index.js
+++ backend/index.js
@@ -47,7 +47,7 @@ app.use('/api/auth', auth);
 
 
 const port = process.env.PORT | 3000;
-app.listen(port, ()=> {
+app.listen(port,'localhost', ()=> {
     console.log("listening on " + port);
 
 });
